b5916d99020fd5a0636f40353c5d27be537b049b
naboychenko <36155666+naboychenko@users.noreply.github.com>
2018-02-11 22:55:01 +0300

    Create Main.java

---
+++ SnailOutputTask/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Viktoria on 09.02.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(3, 4);
+        m.show();
+        m.showSnailOrder();
+    }
+}

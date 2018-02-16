4eb5bdc1a1b689e23e1b8c66ea3e2c83f00c2895
naboychenko <36155666+naboychenko@users.noreply.github.com>
2018-02-16 14:03:19 +0300

    Update Main.java

--- TakeMediaFromCache/src/main/java/Main.java
+++ TakeMediaFromCache/src/main/java/Main.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 /**
  * Created by Viktoria Naboychenko on 15.02.2018.
+ 
+ Программа копирует mp3 файлы из кеша для браузера Chrome в заданную пользователем директорию.
+ Если в метаданных файла есть название трека и исполнителя, то файл в папке назначения будет назван по шаблону
+ "Исполнитель - Название трека.mp3", иначе будет использовано имя по умолчанию "UnknownArtist - UnknownTitle(Number).mp3",
+ где Number порядковый номер трека с именем по умолчанию в папке назначения.
  */
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

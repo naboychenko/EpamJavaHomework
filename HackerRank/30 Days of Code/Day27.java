a09e0860f332bf567abe197a871134d72da2b18e
naboychenko <36155666+naboychenko@users.noreply.github.com>
2018-02-17 02:32:40 +0300

    Create Day27.java

---
+++ HackerRank/30 Days of Code/Day27.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+        System.out.println("4 3");
+        System.out.println("-1 0 4 2");
+        
+        System.out.println("5 5");
+        System.out.println("-1 -3 -4 2 0");
+        
+        System.out.println("6 1");
+        System.out.println("-20 0 4 100 0 0");
+        
+        System.out.println("7 5");
+        System.out.println("-1 -3 4 -2 0 -35 -67");
+        
+        System.out.println("8 3");
+        System.out.println("1 0 100 20 34 89 5 -1");
+        
+    }
+}
